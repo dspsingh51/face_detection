d2c135feb68e9c1cfba8985e50b06805b30a6f4d
Deepak <dspsingh51@gmail.com>
2019-01-08 20:21:43 +0530

    Add files via upload

--- face_detection.py
+++ face_detection.py
@@ -4,16 +4,15 @@
 import cv2 
 
 # load the required trained XML classifiers 
-# https://github.com/Itseez/opencv/blob/master/ 
-# data/haarcascades/haarcascade_frontalface_default.xml 
+
+# that is haarcascade_frontalface_default.xml 
 # Trained XML classifiers describes some features of some 
 # object we want to detect a cascade function is trained 
 # from a lot of positive(faces) and negative(non-faces) 
 # images. 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') 
 
-# https://github.com/Itseez/opencv/blob/master 
-# /data/haarcascades/haarcascade_eye.xml 
+#load haarcascade_eye.xml 
 # Trained XML file for detecting eyes 
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml') 
 
